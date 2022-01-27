f7fc196324b5204fec0ae4eff011759cf40cffc9
Lucas Pozzi <lucaspozzif20@gmail.com>
2022-01-27 08:23:37 -0300

    last commit

--- lib/carrinho.dart
+++ lib/carrinho.dart
@@ -252,7 +252,7 @@ class _RegisterState extends State<Register> {
                           PageRouteBuilder(
                             pageBuilder: (c, a1, a2) => const Registro(),
                             transitionsBuilder: (c, anim, a2, child) =>
-                                FadeTransition(opacity: anim, child: child)
+                                FadeTransition(opacity: anim, child: child),
                             transitionDuration:
                                 const Duration(milliseconds: 100),
                           ),
